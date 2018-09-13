dc548002ac8874a066b5305638b89dc08426e5fd
k-ohshima <kazumasa_ohshima@voyagegroup.com>
2018-09-13 18:52:17 +0900

    Revert "add date swap function"
    This reverts commit a7888f8d6b9f9e40c528bf52fb5b66116d6dbdf7.

--- tests/DateRangeTest.php
+++ tests/DateRangeTest.php
@@ -129,18 +129,13 @@ class DateRangeTest extends \PHPUnit_Framework_TestCase
         new DateRange(['today']);
     }
 
-    /** @test */
-    public function canSwapDates()
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function shouldHaveEndDateIsAfterThanStartDate()
     {
-        $range = new DateRange(['2018-09-13', '2018-08-01']);
-        $this->assertSame(
-            '2018-08-01',
-            $range->getStart()->format('Y-m-d')
-        );
-        $this->assertSame(
-            '2018-09-13',
-            $range->getEnd()->format('Y-m-d')
-        );
+        new DateRange(['tomorrow', 'today']);
     }
 
     /** @test */
